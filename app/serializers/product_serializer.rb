ec4d4d323e27079dfc7d70502199b68278bcbb25
Michael Anderson <michanders@msn.com>
2018-02-22 21:37:38 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -5,4 +5,4 @@ class ProductSerializer
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"price": "' + product.price + '", '
     serialized_product += '"inventory": ' + product.inventory.to_s + ', '
-    serialized_product += '"price": "' + product.price + '", '
+    serialized_product += '"description": "' + product.description + '", '
