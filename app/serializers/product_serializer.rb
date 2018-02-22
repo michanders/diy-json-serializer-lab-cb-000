54fe7fabe936d08079df41d2792c8caf7a1214b1
Michael Anderson <michanders@msn.com>
2018-02-22 21:57:00 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,6 +2,7 @@ class ProductSerializer
   def self.serialize(product)
 
     serialized_product = '{'
+    serialized_product += '"id": ' + product.id.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"price": ' + product.price.to_s + ', '
     serialized_product += '"inventory": ' + product.inventory.to_s + ', '
